46d6015b455be390252c0ac5da701847f83d7f3b
phyranja <phyranja@charon.ch>
2014-06-22 20:43:35 +0200

    Yay, I'modding :P
    Started EssensCrops mod

---
+++ EssenceCrops/ch/phyranja/EssenceCrops/essenses/Essence.java
@@ -0,0 +1,5 @@
+package ch.phyranja.EssenceCrops.essenses;
+
+public enum Essence {
+	NEUTRAL, COAL, REDSTONE, GLOWSTONE, IRON, COPPER, GOLD, DIAMOND, EMERALD
+}
---
+++ EssenceCrops/ch/phyranja/EssenceCrops/lib/Names.java
@@ -0,0 +1,11 @@
+package ch.phyranja.EssenceCrops.lib;
+
+@SuppressWarnings("javadoc")
+public class Names {
+	/*
+	 * All names must be lower case
+	 */
+
+	public static final String NeutralES = "neutral_es";
+	
+}
