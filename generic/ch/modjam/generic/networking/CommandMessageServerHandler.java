00976e40655075256d49de124d691fb5bb2c0f89
judos <judos@gmx.ch>
2014-06-25 16:31:58 +0200

    fixed network issue

--- generic/ch/modjam/generic/networking/CommandMessageServerHandler.java
+++ generic/ch/modjam/generic/networking/CommandMessageServerHandler.java
@@ -1,31 +1,26 @@
 package ch.modjam.generic.networking;
 
-import ch.modjam.generic.GenericMod;
-import ch.modjam.generic.tileEntity.GenericTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import ch.modjam.generic.tileEntity.GenericTileEntity;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 
 /**
- * Server side handler for Command Messages. This will propagate the command and
- * its data to the correct Tile Entity.
+ * Server side handler for Command Messages. This will propagate the command and its data to the
+ * correct Tile Entity.
  * 
  * @author Andreas Waelchli <andreas.waelchli@me.com>
  */
-public class CommandMessageServerHandler implements
-		IMessageHandler<CommandMessage, IMessage> {
+public class CommandMessageServerHandler implements IMessageHandler<CommandMessage, IMessage> {
 
 	@Override
 	public IMessage onMessage(CommandMessage message, MessageContext ctx) {
 		// retreive Tile Entity
-		TileEntity te = FMLCommonHandler
-				.instance()
-				.getMinecraftServerInstance()
-				.worldServerForDimension(message.getDimension())
-				.getTileEntity(message.getXCoord(), message.getYCoord(),
-						message.getZCoord());
+		TileEntity te = FMLCommonHandler.instance().getMinecraftServerInstance()
+			.worldServerForDimension(message.getDimension()).getTileEntity(message.getXCoord(),
+				message.getYCoord(), message.getZCoord());
 		if (!(te instanceof GenericTileEntity)) {
 			System.err.println("TileEntity is no GenericTileEntity");
 			return null;
@@ -34,8 +29,10 @@ public class CommandMessageServerHandler implements
 		// process
 		if (message.getCommand() != "")
 			entity.onNetworkCommand(message.getCommand(), message.getData());
-		NBTMessage updateMessage = new NBTMessage(entity);
-		GenericMod.NETWORK.sendToAll(updateMessage);
+
+		te.getWorldObj().markBlockForUpdate(te.xCoord, te.yCoord, te.zCoord);
+		// NBTMessage updateMessage = new NBTMessage(entity);
+		// GenericMod.NETWORK.sendToAll(updateMessage);
 		// finish
 		return null;
 	}
--- mcmod/ch/judos/mcmod/gui/CustomBoxTE.java
+++ mcmod/ch/judos/mcmod/gui/CustomBoxTE.java
@@ -6,8 +6,12 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.util.StatCollector;
 import ch.judos.mcmod.lib.Names;
+import cpw.mods.fml.common.FMLCommonHandler;
 
 /**
  * @author j
@@ -24,6 +28,18 @@ public class CustomBoxTE extends BoxTE {
 	@Override
 	public String getInventoryName() {
 		return StatCollector.translateToLocal("tile." + Names.CustomBox + ".name");
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
+		super.onDataPacket(net, pkt);
+		System.out.println(FMLCommonHandler.instance().getSide() + " onDataPacket");
+	}
+
+	@Override
+	public Packet getDescriptionPacket() {
+		System.out.println(FMLCommonHandler.instance().getSide() + " getDescriptionPacket");
+		return super.getDescriptionPacket();
 	}
 
 	/**
