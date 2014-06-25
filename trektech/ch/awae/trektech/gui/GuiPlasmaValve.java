35275dc605441a3097a5691fe2c9d24eb29a66bb
Andreas Wälchli <andreas.waelchli@me.com>
2014-06-25 16:15:07 +0200

    broken

--- trektech/ch/awae/trektech/gui/GuiPlasmaValve.java
+++ trektech/ch/awae/trektech/gui/GuiPlasmaValve.java
@@ -1,6 +1,5 @@
 package ch.awae.trektech.gui;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -9,7 +8,6 @@ import net.minecraft.util.StatCollector;
 
 import org.lwjgl.opengl.GL11;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import ch.awae.trektech.entities.TileEntityPlasmaValve;
 import ch.awae.trektech.gui.container.ContainerPlasmaValve;
 import ch.judos.mcmod.lib.Names;
@@ -18,7 +16,7 @@ import ch.judos.mcmod.lib.References;
 /**
  * @author awae
  * 
- * TODO: cleanup, but really
+ *         TODO: cleanup, but really
  */
 public class GuiPlasmaValve extends GuiContainer {
 
@@ -35,6 +33,15 @@ public class GuiPlasmaValve extends GuiContainer {
 	}
 
 	@Override
+	public void initGui() {
+		super.initGui();
+		int x = (this.width - this.xSize) / 2;
+		int y = (this.height - this.ySize) / 2;
+		this.buttonList.add(new GuiButton(0, x + 140, y + 30, 30, 20, "+"));
+		this.buttonList.add(new GuiButton(1, x + 140, y + 50, 30, 20, "-"));
+	}
+
+	@Override
 	protected void drawGuiContainerForegroundLayer(int param1, int param2) {
 		String s = StatCollector.translateToLocal("tile.valve0.name");
 		int color = 4210752;
@@ -44,7 +51,8 @@ public class GuiPlasmaValve extends GuiContainer {
 				StatCollector.translateToLocal("container.inventory"), 8,
 				this.ySize - 96 + 2, color);
 		this.fontRendererObj.drawString(this.tileEntity.getPressureThreshold()
-				+ "", (this.width - this.xSize) / 2 + 120, (this.height - this.ySize) / 2 + 40, color);
+				+ "", (this.width - this.xSize) / 2 + 120,
+				(this.height - this.ySize) / 2 + 40, color);
 	}
 
 	@Override
@@ -56,21 +64,11 @@ public class GuiPlasmaValve extends GuiContainer {
 		int x = (this.width - this.xSize) / 2;
 		int y = (this.height - this.ySize) / 2;
 		this.drawTexturedModalRect(x, y, 0, 0, this.xSize, this.ySize);
-		this.buttonList.add(new GuiButton(0, x + 140, y + 30, 30, 20, "+"));
-		this.buttonList.add(new GuiButton(1, x + 140, y + 50, 30, 20, "-"));
 	}
 
-	private long timeStamp = 0;
-	
 	@Override
 	protected void actionPerformed(GuiButton button) {
 
-		if (Minecraft.getSystemTime() < this.timeStamp + 500)
-			return;
-		
-		System.out.println(FMLCommonHandler.instance().getSide());
-		
-		
 		switch (button.id) {
 		case 0:
 			this.tileEntity.setPressureThreshold(this.tileEntity
@@ -83,7 +81,6 @@ public class GuiPlasmaValve extends GuiContainer {
 		default:
 			break;
 		}
-		this.timeStamp = Minecraft.getSystemTime();
 	}
 
 }
