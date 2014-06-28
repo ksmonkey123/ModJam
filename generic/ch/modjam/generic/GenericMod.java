de29d4dbb0546bf708f2f03f5d0cd194b7b43e83
judos <judos@gmx.ch>
2014-06-28 14:15:34 +0200

    generic mod name added

--- generic/ch/modjam/generic/GenericMod.java
+++ generic/ch/modjam/generic/GenericMod.java
@@ -12,55 +12,55 @@ import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
 
 /**
- * Generic Mod Class. Initializing the generic collection as a mod is required
- * for networking.
+ * Generic Mod Class. Initializing the generic collection as a mod is required for networking.
  * 
  * @author Andreas Waelchli (andreas.waelchli@me.com)
  * @author judos
  */
-@Mod(modid = GenericMod.MODID, version = GenericMod.VERSION)
+@Mod(modid = GenericMod.MODID, version = GenericMod.VERSION, name = GenericMod.NAME)
 public class GenericMod {
-    /**
-     * ModId
-     */
-    public static final String         MODID   = "generic";
-    /**
-     * Version identifier
-     */
-    public static final String         VERSION = "1.0";
-    
-    /**
-     * GenericMod Network Wrapper. Used for TileEntity updates
-     */
-    public static SimpleNetworkWrapper NETWORK;
-    
-    /**
-     * The GenericMod instance
-     */
-    @Instance("generic")
-    public static GenericMod           instance;
-    
-    /**
-     * PreInit
-     * 
-     * @param event
-     */
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        NetworkRegistry.INSTANCE.registerGuiHandler(this,
-                new GenericGuiHandler());
-    }
-    
-    /**
-     * Initialize the mod. sets up the networking system
-     * 
-     * @param event
-     */
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        NETWORK = new SimpleNetworkWrapper("Generic");
-        NETWORK.registerMessage(CommandMessageServerHandler.class,
-                CommandMessage.class, 0, Side.SERVER);
-        
-    }
+	/**
+	 * ModId
+	 */
+	public static final String			MODID	= "generic";
+	/**
+	 * Version identifier
+	 */
+	public static final String			VERSION	= "1.0";
+
+	public static final String			NAME	= "GenericMod";
+
+	/**
+	 * GenericMod Network Wrapper. Used for TileEntity updates
+	 */
+	public static SimpleNetworkWrapper	NETWORK;
+
+	/**
+	 * The GenericMod instance
+	 */
+	@Instance("generic")
+	public static GenericMod			instance;
+
+	/**
+	 * PreInit
+	 * 
+	 * @param event
+	 */
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		NetworkRegistry.INSTANCE.registerGuiHandler(this, new GenericGuiHandler());
+	}
+
+	/**
+	 * Initialize the mod. sets up the networking system
+	 * 
+	 * @param event
+	 */
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		NETWORK = new SimpleNetworkWrapper("Generic");
+		NETWORK.registerMessage(CommandMessageServerHandler.class, CommandMessage.class, 0,
+			Side.SERVER);
+
+	}
 }
