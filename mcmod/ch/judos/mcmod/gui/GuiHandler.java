295e2b2c438ba18a8744829e8ff464bf94d34163
judos <judos@gmx.ch>
2014-06-28 18:27:54 +0200

    generic item gui handler

--- mcmod/ch/judos/mcmod/gui/GuiHandler.java
+++ mcmod/ch/judos/mcmod/gui/GuiHandler.java
@@ -1,9 +1,7 @@
 package ch.judos.mcmod.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-import ch.modjam.generic.tileEntity.IHasGui;
 import cpw.mods.fml.common.network.IGuiHandler;
 
 /**
@@ -14,9 +12,6 @@ public class GuiHandler implements IGuiHandler {
 	@Override
 	public Object getServerGuiElement(int guiID, EntityPlayer player, World world, int x, int y,
 			int z) {
-		TileEntity te = world.getTileEntity(x, y, z);
-		if (te instanceof IHasGui)
-			return ((IHasGui) te).getGuiServer(player.inventory);
 
 		return null;
 	}
@@ -24,9 +19,6 @@ public class GuiHandler implements IGuiHandler {
 	@Override
 	public Object getClientGuiElement(int guiID, EntityPlayer player, World world, int x, int y,
 			int z) {
-		TileEntity te = world.getTileEntity(x, y, z);
-		if (te instanceof IHasGui)
-			return ((IHasGui) te).getGuiClient(player.inventory);
 		return null;
 	}
 
