7c5ed60ab4980e27c3109e2e0a1bd833a205a3f1
judos <judos@gmx.ch>
2014-06-29 17:00:33 +0200

    changed slot to furnaceSlot

--- trektech/ch/awae/trektech/gui/container/ContainerPlasmaFurnace.java
+++ trektech/ch/awae/trektech/gui/container/ContainerPlasmaFurnace.java
@@ -3,30 +3,30 @@ package ch.awae.trektech.gui.container;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Slot;
+import net.minecraft.inventory.SlotFurnace;
 import ch.awae.trektech.entities.TileEntityPlasmaFurnace;
 import ch.modjam.generic.tileEntity.GenericContainer;
 
 @SuppressWarnings("javadoc")
 public class ContainerPlasmaFurnace extends GenericContainer {
-    
-    private TileEntityPlasmaFurnace tileFurnace;
-    
-    public ContainerPlasmaFurnace(InventoryPlayer par1InventoryPlayer,
-            TileEntityPlasmaFurnace te) {
-        super(par1InventoryPlayer);
-        this.tileFurnace = te;
-        this.addSlotToContainer(new Slot(te, 0, 44, 42));
-        this.addSlotToContainer(new Slot(te, 1, 116, 42));
-    }
-    
-    @Override
-    public boolean canInteractWith(EntityPlayer par1EntityPlayer) {
-        return this.tileFurnace.isUseableByPlayer(par1EntityPlayer);
-    }
-    
-    @Override
-    public int getSizeInventory() {
-        return this.tileFurnace.getSizeInventory();
-    }
-    
+
+	private TileEntityPlasmaFurnace	tileFurnace;
+
+	public ContainerPlasmaFurnace(InventoryPlayer inventory, TileEntityPlasmaFurnace te) {
+		super(inventory);
+		this.tileFurnace = te;
+		this.addSlotToContainer(new Slot(te, 0, 44, 42));
+		this.addSlotToContainer(new SlotFurnace(inventory.player, te, 1, 116, 42));
+	}
+
+	@Override
+	public boolean canInteractWith(EntityPlayer par1EntityPlayer) {
+		return this.tileFurnace.isUseableByPlayer(par1EntityPlayer);
+	}
+
+	@Override
+	public int getSizeInventory() {
+		return this.tileFurnace.getSizeInventory();
+	}
+
 }
