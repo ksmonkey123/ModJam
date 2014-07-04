219bf91680ebfcdd496f756eb793fe021cdd2ad3
Andreas Wälchli <andreas.waelchli@me.com>
2014-07-04 23:30:11 +0200

    updated interface

--- generic/ch/modjam/generic/identification/IUniqueIdProvider.java
+++ generic/ch/modjam/generic/identification/IUniqueIdProvider.java
@@ -16,15 +16,17 @@ public interface IUniqueIdProvider {
      * set ID to be used
      * 
      * @param id
+     * @throws IllegalStateException
      */
-    public void useID(int id);
+    public void useID(int id) throws IllegalStateException;
     
     /**
      * set ID to be unused
      * 
      * @param id
+     * @throws IllegalStateException
      */
-    public void freeID(int id);
+    public void freeID(int id) throws IllegalStateException;
     
     /**
      * indicates whether or not an ID is free.
@@ -33,5 +35,12 @@ public interface IUniqueIdProvider {
      * @return <tt>true</tt> if an ID is not used, <tt>false</tt> otherwise
      */
     public boolean isFreeID(int id);
+    
+    /**
+     * indicates whether or not the provider has at least 1 unused ID.
+     * 
+     * @return <tt>
+     */
+    public boolean hasFreeID();
     
 }
