f720436b24767a6f803fb5e78f2553e694667ff8
Julian <judos@students.unibe.ch>
2014-07-09 02:19:22 +0200

    hotfix for some generic multiblock compile errors, added todo tasks instead

--- generic/ch/modjam/generic/multiblock/MultiBlockTileEntity.java
+++ generic/ch/modjam/generic/multiblock/MultiBlockTileEntity.java
@@ -8,170 +8,164 @@ import ch.modjam.generic.tileEntity.GenericTileEntity;
  * @author Andreas Waelchli (andreas.waelchli@me.com)
  */
 public abstract class MultiblockTileEntity extends GenericTileEntity {
-    
-    private EnumTileEntityState state;
-    private long                instanceID;
-    private String              multiblockID;
-    private boolean             isRegistered;
-    
-    /**
-     * instantiate a new MultiblockTileEntity
-     */
-    public MultiblockTileEntity() {
-        this.state = EnumTileEntityState.IDLE;
-        this.instanceID = -1;
-        this.multiblockID = null;
-        this.isRegistered = false;
-    }
-    
-    @Override
-    public final void tick() {
-        switch (this.state) {
-            case MASTER:
-                if (!this.isRegistered)
-                    this.register();
-                this.masterTick();
-                break;
-            case SLAVE:
-                this.slaveTick();
-                break;
-            default:
-                if (!this.isRegistered)
-                    this.isRegistered = false;
-                break;
-        }
-    }
-    
-    private void register() {
-        MultiblockRegistry.instance().registerMultiblockInstance(
-                this.instanceID, this.multiblockID, this);
-    }
-    
-    /**
-     * This ticking method is executed when the TileEntity is a master
-     */
-    public abstract void masterTick();
-    
-    /**
-     * This ticking method is executed when the TileEntity is a slave
-     */
-    public abstract void slaveTick();
-    
-    /**
-     * store custom data
-     * 
-     * @param tag
-     */
-    public abstract void writeCustomNBT(NBTTagCompound tag);
-    
-    /**
-     * restore custom data
-     * 
-     * @param tag
-     */
-    public abstract void readCustomNBT(NBTTagCompound tag);
-    
-    @Override
-    public final void writeNBT(NBTTagCompound tag) {
-        tag.setInteger("EntityState", this.state.ordinal());
-        tag.setLong("InstanceID", this.instanceID);
-        this.writeCustomNBT(tag);
-    }
-    
-    @Override
-    public final void readNBT(NBTTagCompound tag) {
-        this.state = EnumTileEntityState.values()[tag.getInteger("EntityState")];
-        this.instanceID = tag.getLong("InstanceID");
-        this.readCustomNBT(tag);
-    }
-    
-    /**
-     * activate this TileEntity as a slave of a given MultiBlock
-     * 
-     * @param instanceID
-     *            the MultiBlock's instance ID
-     * @throws AlreadyOwnedByMultiblockException
-     *             whenever a TileEntity that is already registered to a
-     *             structure is attempted to be registered to a new one
-     */
-    @SuppressWarnings("hiding")
-    public final void activateAsSlave(long instanceID)
-            throws AlreadyOwnedByMultiblockException {
-        if (this.state != EnumTileEntityState.IDLE)
-            throw new AlreadyOwnedByMultiblockException();
-        this.state = EnumTileEntityState.SLAVE;
-        this.instanceID = instanceID;
-    }
-    
-    /**
-     * resets the multiblock's internal state to idle and removes its
-     * tile-entity registration.
-     */
-    public final void resetToIdle() {
-        this.state = EnumTileEntityState.IDLE;
-        this.instanceID = -1;
-    }
-    
-    /**
-     * @return <tt>true</tt> if the tile-entity is idle and free to be
-     *         registered, <tt>false</tt> otherwise
-     */
-    public boolean isIdle() {
-        return this.state == EnumTileEntityState.IDLE;
-    }
-    
-    /**
-     * activates the tile-entity as the master of the multiblock instance with
-     * the given ID. This also registers all the multiblock slaves to this
-     * entity. In case the multiblock can not be completely registered (e.g.
-     * because one of its TileEntities is already registered to another
-     * structure), the slave registration is reverted. However this is not
-     * protected against multiple access. The <i>later</i> master will simply
-     * fail to register overlapping Entities, even if the <i>first</i> fails
-     * too, which would make the <i>later</i> one valid.
-     * 
-     * @param instanceID
-     * @param multiblockID
-     * @return <tt>true</tt> if structure registration was successful,
-     *         <tt>false</tt> otherwise.
-     */
-    @SuppressWarnings("hiding")
-    public boolean activateAsMaster(long instanceID, String multiblockID) {
-        if (this.state != EnumTileEntityState.IDLE)
-            return false;
-        MultiblockPoint points[] = MultiblockRegistry.instance()
-                .getMultiblockByInstanceID(instanceID).getMultiblockPoints();
-        int counter = 0;
-        try {
-            for (MultiblockPoint pt : points) {
-                int x = pt.getX(this.xCoord);
-                int y = pt.getY(this.yCoord);
-                int z = pt.getZ(this.zCoord);
-                TileEntity te = this.worldObj.getTileEntity(x, y, z);
-                if (te instanceof MultiblockTileEntity)
-                    ((MultiblockTileEntity) te).activateAsSlave(instanceID);
-                counter++;
-            }
-            this.state = EnumTileEntityState.MASTER;
-            this.instanceID = instanceID;
-            this.multiblockID = multiblockID;
-            this.isRegistered = true;
-            return true;
-        } catch (AlreadyOwnedByMultiblockException ex) {
-            // reset previously set slaves
-            for (MultiblockPoint pt : points) {
-                if (counter == 0)
-                    break;
-                int x = pt.getX(this.xCoord);
-                int y = pt.getY(this.yCoord);
-                int z = pt.getZ(this.zCoord);
-                TileEntity te = this.worldObj.getTileEntity(x, y, z);
-                if (te instanceof MultiblockTileEntity)
-                    ((MultiblockTileEntity) te).resetToIdle();
-                counter--;
-            }
-        }
-        return false;
-    }
-    
+
+	private EnumTileEntityState	state;
+	private long				instanceID;
+	private String				multiblockID;
+	private boolean				isRegistered;
+
+	/**
+	 * instantiate a new MultiblockTileEntity
+	 */
+	public MultiblockTileEntity() {
+		this.state = EnumTileEntityState.IDLE;
+		this.instanceID = -1;
+		this.multiblockID = null;
+		this.isRegistered = false;
+	}
+
+	@Override
+	public final void tick() {
+		switch (this.state) {
+			case MASTER:
+				if (!this.isRegistered)
+					this.register();
+				this.masterTick();
+				break;
+			case SLAVE:
+				this.slaveTick();
+				break;
+			default:
+				if (!this.isRegistered)
+					this.isRegistered = false;
+				break;
+		}
+	}
+
+	private void register() {
+		// TODO: fix the following which caused compile errors
+		// MultiblockRegistry.instance().registerMultiblockInstance(
+		// this.instanceID, this.multiblockID, this);
+	}
+
+	/**
+	 * This ticking method is executed when the TileEntity is a master
+	 */
+	public abstract void masterTick();
+
+	/**
+	 * This ticking method is executed when the TileEntity is a slave
+	 */
+	public abstract void slaveTick();
+
+	/**
+	 * store custom data
+	 * 
+	 * @param tag
+	 */
+	public abstract void writeCustomNBT(NBTTagCompound tag);
+
+	/**
+	 * restore custom data
+	 * 
+	 * @param tag
+	 */
+	public abstract void readCustomNBT(NBTTagCompound tag);
+
+	@Override
+	public final void writeNBT(NBTTagCompound tag) {
+		tag.setInteger("EntityState", this.state.ordinal());
+		tag.setLong("InstanceID", this.instanceID);
+		this.writeCustomNBT(tag);
+	}
+
+	@Override
+	public final void readNBT(NBTTagCompound tag) {
+		this.state = EnumTileEntityState.values()[tag.getInteger("EntityState")];
+		this.instanceID = tag.getLong("InstanceID");
+		this.readCustomNBT(tag);
+	}
+
+	/**
+	 * activate this TileEntity as a slave of a given MultiBlock
+	 * 
+	 * @param instanceID the MultiBlock's instance ID
+	 * @throws AlreadyOwnedByMultiblockException whenever a TileEntity that is already registered to
+	 *             a structure is attempted to be registered to a new one
+	 */
+	@SuppressWarnings("hiding")
+	public final void activateAsSlave(long instanceID) throws AlreadyOwnedByMultiblockException {
+		if (this.state != EnumTileEntityState.IDLE)
+			throw new AlreadyOwnedByMultiblockException();
+		this.state = EnumTileEntityState.SLAVE;
+		this.instanceID = instanceID;
+	}
+
+	/**
+	 * resets the multiblock's internal state to idle and removes its tile-entity registration.
+	 */
+	public final void resetToIdle() {
+		this.state = EnumTileEntityState.IDLE;
+		this.instanceID = -1;
+	}
+
+	/**
+	 * @return <tt>true</tt> if the tile-entity is idle and free to be registered, <tt>false</tt>
+	 *         otherwise
+	 */
+	public boolean isIdle() {
+		return this.state == EnumTileEntityState.IDLE;
+	}
+
+	/**
+	 * activates the tile-entity as the master of the multiblock instance with the given ID. This
+	 * also registers all the multiblock slaves to this entity. In case the multiblock can not be
+	 * completely registered (e.g. because one of its TileEntities is already registered to another
+	 * structure), the slave registration is reverted. However this is not protected against
+	 * multiple access. The <i>later</i> master will simply fail to register overlapping Entities,
+	 * even if the <i>first</i> fails too, which would make the <i>later</i> one valid.
+	 * 
+	 * @param instanceID
+	 * @param multiblockID
+	 * @return <tt>true</tt> if structure registration was successful, <tt>false</tt> otherwise.
+	 */
+	@SuppressWarnings("hiding")
+	public boolean activateAsMaster(long instanceID, String multiblockID) {
+		if (this.state != EnumTileEntityState.IDLE)
+			return false;
+		MultiblockPoint points[] = MultiblockRegistry.instance().getMultiblockByInstanceID(
+			instanceID).getMultiblockPoints();
+		int counter = 0;
+		try {
+			for (MultiblockPoint pt : points) {
+				int x = pt.getX(this.xCoord);
+				int y = pt.getY(this.yCoord);
+				int z = pt.getZ(this.zCoord);
+				TileEntity te = this.worldObj.getTileEntity(x, y, z);
+				if (te instanceof MultiblockTileEntity)
+					((MultiblockTileEntity) te).activateAsSlave(instanceID);
+				counter++;
+			}
+			this.state = EnumTileEntityState.MASTER;
+			this.instanceID = instanceID;
+			this.multiblockID = multiblockID;
+			this.isRegistered = true;
+			return true;
+		} catch (AlreadyOwnedByMultiblockException ex) {
+			// reset previously set slaves
+			for (MultiblockPoint pt : points) {
+				if (counter == 0)
+					break;
+				int x = pt.getX(this.xCoord);
+				int y = pt.getY(this.yCoord);
+				int z = pt.getZ(this.zCoord);
+				TileEntity te = this.worldObj.getTileEntity(x, y, z);
+				if (te instanceof MultiblockTileEntity)
+					((MultiblockTileEntity) te).resetToIdle();
+				counter--;
+			}
+		}
+		return false;
+	}
+
 }
--- generic/ch/modjam/generic/multiblock/MultiblockRegistry.java
+++ generic/ch/modjam/generic/multiblock/MultiblockRegistry.java
@@ -9,133 +9,126 @@ import java.util.HashMap;
  * @version 1
  */
 public class MultiblockRegistry {
-    
-    private final static MultiblockRegistry INSTANCE   = new MultiblockRegistry();
-    private final static Object             SYNC_TOKEN = new Object();
-    
-    /**
-     * @return the registry instance
-     */
-    public static MultiblockRegistry instance() {
-        return MultiblockRegistry.INSTANCE;
-    }
-    
-    private HashMap<String, Multiblock> multiblocks       = new HashMap<String, Multiblock>();
-    
-    private HashMap<Integer, ActiveSet>    activeMultiblocks = new HashMap<Integer, ActiveSet>();
-    
-    private long                        maxActiveID       = 0;
-    
-    /**
-     * register a multiblock structure as a valid multiblock
-     * 
-     * @param structure
-     * @param id
-     */
-    public void registerMultiblock(Multiblock structure, String id) {
-        // FIXME: prevent duplicate structure ID's
-        this.multiblocks.put(id, structure);
-    }
-    
-    /**
-     * retrieves an active multiblock over its ID
-     * 
-     * @param instanceID
-     * @return the multiblock
-     */
-    public Multiblock getMultiblockByInstanceID(long instanceID) {
-        return this.multiblocks.get(this.activeMultiblocks.get(
-                Long.valueOf(instanceID)).getMultiblock());
-    }
-    
-    /**
-     * retrieves the master tile entity of an active multiblock over its ID
-     * 
-     * @param instanceID
-     * @return the master tile entity
-     */
-    public MultiblockTileEntity getMultiblockTileEntityByInstanceID(
-            long instanceID) {
-        return this.activeMultiblocks.get(Long.valueOf(instanceID))
-                .getTileEntity();
-    }
-    
-    /**
-     * register a new multiblock instance with an internally generated id.
-     * 
-     * @param structure
-     * @param te
-     *            the multiblock's master tile-entity
-     * @return the generated instance id. use this ID for multiblock references
-     */
-    public int registerMultiblockInstance(String structure,
-            MultiblockTileEntity te) {
-        int id;
-        synchronized (MultiblockRegistry.SYNC_TOKEN) {
-            //id = ++this.maxActiveID;
-            //this.registerMultiblockInstance(id, structure, te);
-        }
-        return id;
-    }
-    
-    /**
-     * @param id
-     * @return the multiblock registered under the given ID
-     */
-    public Multiblock getMultiblockByStructureID(String id) {
-        return this.multiblocks.get(id);
-    }
-    
-    /**
-     * @return an array containing all registered multiblock structure IDs
-     */
-    public String[] getMultiblockStructureIDs() {
-        return this.multiblocks.keySet().toArray(new String[0]);
-    }
-    
-    /**
-     * register a multiblock structure instance with a predefined key. This is
-     * used for world reload to restore the registry. new instances should never
-     * be registered with a preset ID!
-     * 
-     * @param instanceID
-     * @param structure
-     * @param te
-     */
-    public void registerMultiblockInstance(int instanceID, String structure,
-            MultiblockTileEntity te) {
-        this.activeMultiblocks.put(Integer.valueOf(instanceID), new ActiveSet(
-                structure, te));
-        if (this.maxActiveID < instanceID)
-            this.maxActiveID = instanceID;
-    }
-    
-    public int getNextID() {
-        return 0; //this.maxActiveID + 1;
-    }
-    
-    @SuppressWarnings("javadoc")
-    public Multiblock[] getRegisteredMultiblocks() {
-        return this.multiblocks.values().toArray(new Multiblock[0]);
-    }
-    
-    class ActiveSet {
-        private String               multiblock;
-        private MultiblockTileEntity tileEntity;
-        
-        public ActiveSet(String multiblock, MultiblockTileEntity tileEntity) {
-            super();
-            this.multiblock = multiblock;
-            this.tileEntity = tileEntity;
-        }
-        
-        public String getMultiblock() {
-            return this.multiblock;
-        }
-        
-        public MultiblockTileEntity getTileEntity() {
-            return this.tileEntity;
-        }
-        
-    }
+
+	private final static MultiblockRegistry	INSTANCE	= new MultiblockRegistry();
+	private final static Object				SYNC_TOKEN	= new Object();
+
+	/**
+	 * @return the registry instance
+	 */
+	public static MultiblockRegistry instance() {
+		return MultiblockRegistry.INSTANCE;
+	}
+
+	private HashMap<String, Multiblock>	multiblocks			= new HashMap<String, Multiblock>();
+
+	private HashMap<Integer, ActiveSet>	activeMultiblocks	= new HashMap<Integer, ActiveSet>();
+
+	private long						maxActiveID			= 0;
+
+	/**
+	 * register a multiblock structure as a valid multiblock
+	 * 
+	 * @param structure
+	 * @param id
+	 */
+	public void registerMultiblock(Multiblock structure, String id) {
+		// FIXME: prevent duplicate structure ID's
+		this.multiblocks.put(id, structure);
+	}
+
+	/**
+	 * retrieves an active multiblock over its ID
+	 * 
+	 * @param instanceID
+	 * @return the multiblock
+	 */
+	public Multiblock getMultiblockByInstanceID(long instanceID) {
+		return this.multiblocks.get(this.activeMultiblocks.get(Long.valueOf(instanceID))
+			.getMultiblock());
+	}
+
+	/**
+	 * retrieves the master tile entity of an active multiblock over its ID
+	 * 
+	 * @param instanceID
+	 * @return the master tile entity
+	 */
+	public MultiblockTileEntity getMultiblockTileEntityByInstanceID(long instanceID) {
+		return this.activeMultiblocks.get(Long.valueOf(instanceID)).getTileEntity();
+	}
+
+	/**
+	 * register a new multiblock instance with an internally generated id.
+	 * 
+	 * @param structure
+	 * @param te the multiblock's master tile-entity
+	 * @return the generated instance id. use this ID for multiblock references
+	 */
+	public int registerMultiblockInstance(String structure, MultiblockTileEntity te) {
+		int id = 0; // TODO: initialize
+		synchronized (MultiblockRegistry.SYNC_TOKEN) {
+			// id = ++this.maxActiveID;
+			// this.registerMultiblockInstance(id, structure, te);
+		}
+		return id;
+	}
+
+	/**
+	 * @param id
+	 * @return the multiblock registered under the given ID
+	 */
+	public Multiblock getMultiblockByStructureID(String id) {
+		return this.multiblocks.get(id);
+	}
+
+	/**
+	 * @return an array containing all registered multiblock structure IDs
+	 */
+	public String[] getMultiblockStructureIDs() {
+		return this.multiblocks.keySet().toArray(new String[0]);
+	}
+
+	/**
+	 * register a multiblock structure instance with a predefined key. This is used for world reload
+	 * to restore the registry. new instances should never be registered with a preset ID!
+	 * 
+	 * @param instanceID
+	 * @param structure
+	 * @param te
+	 */
+	public void registerMultiblockInstance(int instanceID, String structure, MultiblockTileEntity te) {
+		this.activeMultiblocks.put(Integer.valueOf(instanceID), new ActiveSet(structure, te));
+		if (this.maxActiveID < instanceID)
+			this.maxActiveID = instanceID;
+	}
+
+	public int getNextID() {
+		return 0; // this.maxActiveID + 1;
+	}
+
+	@SuppressWarnings("javadoc")
+	public Multiblock[] getRegisteredMultiblocks() {
+		return this.multiblocks.values().toArray(new Multiblock[0]);
+	}
+
+	class ActiveSet {
+		private String					multiblock;
+		private MultiblockTileEntity	tileEntity;
+
+		public ActiveSet(String multiblock, MultiblockTileEntity tileEntity) {
+			super();
+			this.multiblock = multiblock;
+			this.tileEntity = tileEntity;
+		}
+
+		public String getMultiblock() {
+			return this.multiblock;
+		}
+
+		public MultiblockTileEntity getTileEntity() {
+			return this.tileEntity;
+		}
+
+	}
 }
