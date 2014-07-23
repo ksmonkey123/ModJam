f49f3171215267c629468bdfa8dacef612c7ccee
Julian <judos@students.unibe.ch>
2014-07-23 14:34:59 +0200

    Please update to Forge 1.7.10-10.13.0.1187 with the new MC version

---
+++ test/Note.java
@@ -0,0 +1,17 @@
+/**
+ * FIXME: this note is just to let you know, that we should all update to MC 1.7.10 with the new
+ * latest forge version. This is done as follows: <br>
+ * <br>
+ * 1. change line 25 in the file build.gradle to match the following line:<br>
+ * version = "1.7.10-10.13.0.1187"<br>
+ * <br>
+ * 2. in the same directory run the following shell command:<br>
+ * gradlew setupDecompWorkspace<br>
+ * 
+ * @author j
+ *
+ *
+ */
+public class Note {
+	// see class comment above
+}
