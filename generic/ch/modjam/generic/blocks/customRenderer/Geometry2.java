0eb7b0f2c5a1df719f3a80ca60cf7710ec59510e
judos <judos@gmx.ch>
2014-07-30 20:45:36 +0200

    added quad functionality to the geometry class

--- generic/ch/modjam/generic/blocks/customRenderer/Geometry2.java
+++ generic/ch/modjam/generic/blocks/customRenderer/Geometry2.java
@@ -33,17 +33,18 @@ public class Geometry2 extends Geometry {
 		super(texture);
 	}
 
-	public void addQuadOnSide(double x, double y, double z, double width, double height, EFace visibleFrom) {
+	public void addQuadOnSide(double x, double y, double z, double width, double height,
+			EFace visibleFrom) {
 		addQuadOnSideWithTexRot(x, y, z, width, height, visibleFrom, visibleFrom.getTileIndex(), 0);
 	}
 
-	public void addQuadOnSideWithTex(double x, double y, double z, double width, double height, EFace visibleFrom,
-			int useTextureTileIndex) {
+	public void addQuadOnSideWithTex(double x, double y, double z, double width, double height,
+			EFace visibleFrom, int useTextureTileIndex) {
 		addQuadOnSideWithTexRot(x, y, z, width, height, visibleFrom, useTextureTileIndex, 0);
 	}
 
-	public void addQuadOnSideWithTexRot(double x, double y, double z, double width, double height, EFace visibleFrom,
-			int useTextureTileIndex, int texRotation) {
+	public void addQuadOnSideWithTexRot(double x, double y, double z, double width, double height,
+			EFace visibleFrom, int useTextureTileIndex, int texRotation) {
 
 		// calculates the absolute coordinates of the points that are offset by width,height
 		double[] delta = size2dTo3dDeltaCoordinates(visibleFrom, width, height);
@@ -55,16 +56,60 @@ public class Geometry2 extends Geometry {
 
 		double us = 0, vs = 0, dU, dV;
 
-		boolean swaped = (texRotation % 2) == 1;
+		boolean[] swaped = { (texRotation % 2) == 1 };
 
 		int[] uc, vc; // {value (+delta[n]/2 or -delta[n]/2), coordinate
 						// (0=x,1=y,2=z)}
 
+		int[][] ucvc = calcUcVcFromSide(visibleFrom, swaped);
+		uc = ucvc[0];
+		vc = ucvc[1];
+
+		int rot = texRotation;
+		while (rot > 0) {
+			rot--;
+			int[] temp = uc;
+			uc = arr(vc[0], vc[1]);
+			vc = arr(1 - temp[0], temp[1]);
+		}
+
+		int tx = useTextureTileIndex % 4;
+		int ty = useTextureTileIndex / 4;
+		us = tx * uvPerTile + (0.5 + coord[uc[1]][uc[0]]) * uvPerTile;
+		vs = ty * uvPerTile + (0.5 + coord[vc[1]][vc[0]]) * uvPerTile;
+
+		if (!swaped[0]) {
+			dU = width * uvPerTile;
+			dV = height * uvPerTile;
+		} else {
+			dU = height * uvPerTile;
+			dV = width * uvPerTile;
+		}
+
+		double up = us + dU;
+		double vp = vs + dV;
+
+		double[] u = { us, us, up, up };
+		double[] v = { vs, vp, vp, vs };
+
+		// get the final coordinates for all points of the quad
+		double[][] c = getFinalCoordinatesFromMinMaxSide(visibleFrom, x, xp, y, yp, z, zp);
+		double[] cx = c[0];
+		double[] cy = c[1];
+		double[] cz = c[2];
+
+		for (int i = 0; i < 4; i++)
+			this.addPoint((float) cx[i], (float) cy[i], (float) cz[i],
+				(float) u[(i + texRotation) % 4], (float) v[(i + texRotation) % 4]);
+	}
+
+	private int[][] calcUcVcFromSide(EFace visibleFrom, boolean[] swaped) {
+		int[] uc, vc;
 		switch (visibleFrom) {
 			case TOP: {
 				uc = arr(0, Axis.X.index);
 				vc = arr(0, Axis.Z.index);
-				swaped = !swaped;
+				swaped[0] = !swaped[0];
 				break;
 			}
 			case FRONT: {
@@ -90,50 +135,14 @@ public class Geometry2 extends Geometry {
 			case BOTTOM: {
 				uc = arr(0, Axis.X.index);
 				vc = arr(1, Axis.Z.index);
-				swaped = !swaped;
+				swaped[0] = !swaped[0];
 				break;
 			}
 			default: {
 				throw new RuntimeException("Unhandled enum type: " + visibleFrom);
 			}
 		}
-
-		int rot = texRotation;
-		while (rot > 0) {
-			rot--;
-			int[] temp = uc;
-			uc = arr(vc[0], vc[1]);
-			vc = arr(1 - temp[0], temp[1]);
-		}
-
-		int tx = useTextureTileIndex % 4;
-		int ty = useTextureTileIndex / 4;
-		us = tx * uvPerTile + (0.5 + coord[uc[1]][uc[0]]) * uvPerTile;
-		vs = ty * uvPerTile + (0.5 + coord[vc[1]][vc[0]]) * uvPerTile;
-
-		if (!swaped) {
-			dU = width * uvPerTile;
-			dV = height * uvPerTile;
-		} else {
-			dU = height * uvPerTile;
-			dV = width * uvPerTile;
-		}
-
-		double up = us + dU;
-		double vp = vs + dV;
-
-		double[] u = { us, us, up, up };
-		double[] v = { vs, vp, vp, vs };
-
-		// get the final coordinates for all points of the quad
-		double[][] c = getFinalCoordinatesFromMinMaxSide(visibleFrom, x, xp, y, yp, z, zp);
-		double[] cx = c[0];
-		double[] cy = c[1];
-		double[] cz = c[2];
-
-		for (int i = 0; i < 4; i++)
-			this.addPoint((float) cx[i], (float) cy[i], (float) cz[i],
-				(float) u[(i + texRotation) % 4], (float) v[(i + texRotation) % 4]);
+		return new int[][] { uc, vc };
 	}
 
 	/**
