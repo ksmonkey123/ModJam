fabec869212beb6c6b16796ec7a2f2c5bc28906a
judos <judos@gmx.ch>
2014-08-11 17:49:30 +0200

    improved rendering for station

--- aerialtransports/ch/judos/at/renderer/StationRenderer.java
+++ aerialtransports/ch/judos/at/renderer/StationRenderer.java
@@ -8,6 +8,8 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector3f;
 
 import ch.judos.at.te.TEStation;
+import ch.modjam.generic.blocks.EFace;
+import ch.modjam.generic.blocks.customRenderer.Geometry2;
 import ch.modjam.generic.blocks.customRenderer.Geometry3;
 
 public class StationRenderer extends TileEntitySpecialRenderer {
@@ -27,15 +29,34 @@ public class StationRenderer extends TileEntitySpecialRenderer {
 		TEStation t = (TEStation) ent;
 		// SETUP
 		GL11.glTranslated(transX + 0.5, transY + 0.5, transZ + 0.5);
-
-		r.clear();
-
 		int time = (int) (ent.getWorldObj().getTotalWorldTime());
 		float angle = (float) time / 50;
 
+		r.clear();
+		r.subParts.clear();
+		r.setUvModeScaled(false);
 		r.transform.rotate(angle, new Vector3f(0, 1, 0));
 
-		r.addCubeOfRadius(0.35);
+		final double s = 0.415; // side of one quad for an 8-sided cylinder
+		for (int i = 0; i < 8; i++) {
+			Geometry2 g = new Geometry2(r);
+			g.setUvModeScaled(false);
+			g.transform.rotate((float) (i * 2 * Math.PI / 8), new Vector3f(0, 1, 0));
+			g.addQuadOnSide(-s / 2, -0.5, -0.5, s, 1, EFace.FRONT);
+
+			double us = 1. / 4;
+			double ue = 2. / 4;
+			double vs = 0;
+			double ve = 1. / 4;
+			g.addPoint(-s / 2f, 0.5f, 0, us, vs);
+			g.addPoint(s / 2f, 0.5f, 0, ue, vs);
+			g.addPoint(s / 2f, 0.49f, -0.5f, ue, ve);
+			g.addPoint(-s / 2f, 0.5f, -0.5f, us, ve);
+
+			// g.addQuadOnSide(-s / 2, 0.5, -0.5, s, 0.5, EFace.TOP);
+		}
+
+		// r.addQuadOnSide(-0.36, -0.36, -0.36, 2 * 0.36, 0.36, EFace.FRONT);
 		r.draw(Tessellator.instance);
 		// TEAR DOWN
 		GL11.glTranslated(-transX - 0.5, -transY - 0.5, -transZ - 0.5);
