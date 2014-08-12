6704c73e70fd63c8ea6614ac35a968e085a2f81b
judos <judos@gmx.ch>
2014-08-12 02:43:11 +0200

    item renderer

---
+++ generic/ch/modjam/generic/rendering/ItemRendering.java
@@ -0,0 +1,50 @@
+package ch.modjam.generic.rendering;
+
+import static org.lwjgl.opengl.GL11.glDepthMask;
+import static org.lwjgl.opengl.GL11.glPopMatrix;
+import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.glRotatef;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+
+public class ItemRendering {
+
+	// public static void render3DItem(ItemStack items,float partialTickTime,boolean rotate) {
+	//
+	// }
+
+	// public static void render3DItem(EntityItem item, float partialTickTime, boolean rotate) {
+	// float time = Minecraft.getMinecraft().theWorld.getTotalWorldTime() + partialTickTime;
+	// float bouncetime = time % 63;
+	//
+	// float rot = 1 * (time % 180) - 90;
+	// glPushMatrix();
+	// glDepthMask(true);
+	// if (rotate) {
+	// glRotatef(rot, 0, 1, 0);
+	// }
+	// item.hoverStart = 0.0F;
+	// RenderManager.instance.renderEntityWithPosYaw(item, 0.0D, 1.0D, 0.0D, 0, bouncetime);
+	// glPopMatrix();
+	// }
+
+	public static void render3DItem(ItemStack s, float partialTickTime, boolean rotate) {
+		render3DItem(new EntityItem(Minecraft.getMinecraft().theWorld, 0, 0, 0, s),
+			partialTickTime, rotate);
+	}
+
+	public static void render3DItem(EntityItem item, float partialTickTime, boolean rotate) {
+		float rot = (Minecraft.getMinecraft().theWorld.getTotalWorldTime() + partialTickTime) % 360 * 1.5f;
+		glPushMatrix();
+		glDepthMask(true);
+		rotate &= Minecraft.getMinecraft().gameSettings.fancyGraphics;
+		if (rotate) {
+			glRotatef(rot, 0, 1, 0);
+		}
+		item.hoverStart = 0.0F;
+		RenderManager.instance.renderEntityWithPosYaw(item, 0.0D, 1.0D, 0.0D, 0f, 0f);
+		glPopMatrix();
+	}
+}
