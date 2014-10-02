c224e50683854365ade3e94ba811016d9931396a
judos <judos@gmx.ch>
2014-10-02 02:14:50 +0200

    bug-fix against mc crashing when bound-heart activates empty container

--- aerialtransports/ch/judos/at/gearbox/gui/ContainerGearbox.java
+++ aerialtransports/ch/judos/at/gearbox/gui/ContainerGearbox.java
@@ -2,8 +2,11 @@ package ch.judos.at.gearbox.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
 import ch.judos.at.gearbox.TEStationGearbox;
 import ch.modjam.generic.gui.GenericContainer;
+import ch.modjam.generic.inventory.GenericInventory;
 
 public class ContainerGearbox extends GenericContainer {
 
@@ -11,10 +14,31 @@ public class ContainerGearbox extends GenericContainer {
 		super();
 	}
 
+	/*
+	 * important bug-fix, usually minecraft crashes when items with changing nbt activate a block
+	 * with zero-slots (e.g. bound-heart right-click on gearbox block)
+	 */
 	@Override
 	public int getSizeInventory() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
+	}
+
+	/*
+	 * important bug-fix, usually minecraft crashes when items with changing nbt activate a block
+	 * with zero-slots (e.g. bound-heart right-click on gearbox block)
+	 */
+	@Override
+	public Slot getSlotFromInventory(IInventory p_75147_1_, int p_75147_2_) {
+		return new Slot(new GenericInventory(1, "bug preventer"), 0, 0, 0);
+	}
+
+	/*
+	 * important bug-fix, usually minecraft crashes when items with changing nbt activate a block
+	 * with zero-slots (e.g. bound-heart right-click on gearbox block)
+	 */
+	@Override
+	public Slot getSlot(int p_75139_1_) {
+		return new Slot(new GenericInventory(1, "bug preventer"), 0, 0, 0);
 	}
 
 	@Override
